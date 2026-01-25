88694ceda83441b12e0bad291ea42f7f1885ed0e
WAVEIOVI <waveio.tn@gmail.com>
2026-01-25 15:30:32 +0100

    fix: remove invalid historyApiFallback from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ocean_security_platform/',
-  server: {
-    historyApiFallback: true
-  }
 })
